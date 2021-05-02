d3a394d18bdb33c2e476dd392f554c0ecd296ce5
Nikhil Kumar Gupta <niks.a3198@gmail.com>
2021-05-02 16:29:09 +0530

    added search-bar and particleJS

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const container = document.querySelector(".container");
+const input = document.querySelector("input");
+
+let usersArray = [];
+
+const createCardList = (array) => {
+  container.innerHTML = "";
+
+  array.forEach((obj) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `<div class="name">Name</div><div class="name-content">${obj.username}</div><div class="email">Email</div><div class="email-content">${obj.email}</div>`;
+    container.appendChild(card);
+  });
+};
 
 fetch("http://jsonplaceholder.typicode.com/users")
   .then((data) => {
@@ -8,12 +23,22 @@ fetch("http://jsonplaceholder.typicode.com/users")
   })
   .then((result) => {
     console.log(JSON.parse(result));
-    const array = JSON.parse(result);
-    array.map((obj, index) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-
-      card.innerHTML = `<div class="name">Name</div><div class="name-content">${obj.username}</div><div class="email">Email</div><div class="email-content">${obj.email}</div>`;
-      container.appendChild(card);
-    });
+    usersArray = JSON.parse(result);
+    createCardList(usersArray);
   });
+
+input.addEventListener("input", (event) => {
+  const searchStr = event.target.value.toLowerCase();
+
+  const filteredArray = usersArray.filter((ele) => {
+    return (
+      ele.username.toLowerCase().includes(searchStr) ||
+      ele.email.toLowerCase().includes(searchStr)
+    );
+  });
+
+  createCardList(filteredArray);
+});
+
+// particle js configuration
+particlesJS.load("particles-js", "particles.json");
